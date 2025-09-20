b3d95f6ad0df597f0a21359968f2db7e18be1c15
Xe_xmre <Xe_xmre@outlook.com>
2025-09-20 21:44:05 +0800

    优化样式，更改插入段落的位置

--- js/js-test.js
+++ js/js-test.js
@@ -1,7 +1,9 @@
 function createParagraph() {
   const para = document.createElement("p");
   para.textContent = "你点击了按钮！";
-  document.body.appendChild(para);
+  const box1 = document.querySelector(".box-1"); // 找到 class="box-1" 的元素
+  box1.appendChild(para);  // 把段落插入到它的末尾
+
 }
 
 const buttons = document.querySelectorAll("button");
