5edeb532ba1926247c5aed2e4cc99b064a0379e9
Xe_xmre <Xe_xmre@outlook.com>
2025-09-20 21:08:04 +0800

    修复了js加载顺序的问题

--- js/js-test.js
+++ js/js-test.js
@@ -1,13 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-  function createParagraph() {
-    const para = document.createElement("p");
-    para.textContent = "你点击了按钮！";
-    document.body.appendChild(para);
-  }
+function createParagraph() {
+  const para = document.createElement("p");
+  para.textContent = "你点击了按钮！";
+  document.body.appendChild(para);
+}
 
-  const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("button");
 
-  for (const button of buttons) {
-    button.addEventListener("click", createParagraph);
-  }
-});
+for (const button of buttons) {
+  button.addEventListener("click", createParagraph);
+}
