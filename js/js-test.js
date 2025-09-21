e12e1d2e42f4b3a18700b8a4d7578a371becc22d
Xexmre <Xe_xmre@outlook.com>
2025-09-21 14:13:09 +0800

    修改了循环逻辑，使其更像C。添加了一个页面用以学习。

--- js/js-test.js
+++ js/js-test.js
@@ -1,5 +1,5 @@
 function createParagraph() {
-  const para = document.createElement("p");
+  const para = document.createElement("pdiv");
   para.textContent = "你点击了按钮！";
   const box1 = document.querySelector(".box-1"); // 找到 class="box-1" 的元素
   box1.appendChild(para);  // 把段落插入到它的末尾
@@ -8,6 +8,6 @@ function createParagraph() {
 
 const buttons = document.querySelectorAll("button");
 
-for (const button of buttons) {
-  button.addEventListener("click", createParagraph);
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", createParagraph);
 }
